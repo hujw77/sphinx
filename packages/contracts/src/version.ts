1dd44b372c1e7bf44b4f3762191225cad599e1e9
echo <hujw77@gmail.com>
2024-05-17 22:44:53 +0800

    all: v0.1.1

--- packages/contracts/src/version.ts
+++ packages/contracts/src/version.ts
@@ -1,1 +1,1 @@
-export const CONTRACTS_LIBRARY_VERSION = 'v0.1.0'
+export const CONTRACTS_LIBRARY_VERSION = 'v0.1.1'
--- packages/plugins/src/cli/version.ts
+++ packages/plugins/src/cli/version.ts
@@ -1,1 +1,1 @@
-export const SPHINX_PLUGINS_VERSION = 'v0.1.0'
+export const SPHINX_PLUGINS_VERSION = 'v0.1.1'
