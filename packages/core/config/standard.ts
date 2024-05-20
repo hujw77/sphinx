d143c61e48c528846013b384f1efc1c8d1d564f0
echo <hujw77@gmail.com>
2024-05-20 15:35:37 +0800

    core: deploy on - Ethereum - BNB Smart Chain (aka BSC) - Gnosis Chain - Base - Scroll - Mantle - Darwinia - Crab - Astar zkEVM

--- packages/core/config/standard.ts
+++ packages/core/config/standard.ts
@@ -1,7 +1,10 @@
 import { SphinxSystemConfig } from '../src'
 
 const config: SphinxSystemConfig = {
-  relayers: ['0xd70a2e6eacbdeda77a5d4bbae3bc70239a0e088f'],
+  relayers: [
+    '0xd70a2e6eacbdeda77a5d4bbae3bc70239a0e088f',
+    '0x01978e9f9460a11553151cABB33eF70F65CB000E',
+  ],
 }
 
 export default config
